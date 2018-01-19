f7c827e86286ca7245e76dd72ebd5f99f1d2472a
arranlomas <lomasarran1993@googlemail.com>
2018-01-19 23:43:52 +1000

    extract stuff for reducer

--- app/src/main/java/com/arranlomas/mvisample/todolist/functions/ListReducer.kt
+++ app/src/main/java/com/arranlomas/mvisample/todolist/functions/ListReducer.kt
@@ -1,17 +1,16 @@
 package com.arranlomas.mvisample.todolist.functions
 
+import com.arranlomas.kontent.commons.functions.KontentReducer
+import com.arranlomas.kontent.commons.objects.mvi.KontentResult
+import com.arranlomas.kontent.commons.objects.mvi.KontentViewState
 import com.arranlomas.mvisample.todolist.objects.TodoListResult
 import com.arranlomas.mvisample.todolist.objects.TodoListViewState
 import io.reactivex.functions.BiFunction
 
-/**
- * Created by arran on 5/12/2017.
- */
-internal val listReducer = BiFunction <TodoListViewState, TodoListResult, TodoListViewState> {
-    previousState: TodoListViewState, resultTodo: TodoListResult ->
-    when (resultTodo) {
-        is TodoListResult.ListLoadSuccess -> previousState.copy(items = resultTodo.items, loading = false)
-        is TodoListResult.Error -> previousState.copy(items = emptyList(), loading = false, error = resultTodo.error)
+internal val listReducer = KontentReducer<TodoListResult, TodoListViewState>({ result, previousState ->
+    when (result) {
+        is TodoListResult.ListLoadSuccess -> previousState.copy(items = result.items, loading = false)
+        is TodoListResult.Error -> previousState.copy(items = emptyList(), loading = false, error = result.error)
         is TodoListResult.ListLoadInflight -> previousState.copy(items = emptyList(), loading = true, error = null)
     }
-}
+})
