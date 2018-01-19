1ad66f3108725c6a1025c42883a18673e89b2cd0
arranlomas <lomasarran1993@googlemail.com>
2018-01-19 23:38:16 +1000

    create base network processor

--- app/src/main/java/com/arranlomas/mvisample/todolist/functions/ListActionProcessor.kt
+++ app/src/main/java/com/arranlomas/mvisample/todolist/functions/ListActionProcessor.kt
@@ -1,41 +1,34 @@
 package com.arranlomas.mvisample.todolist.functions
 
-import com.arranlomas.kontent.commons.functions.KontentSimpleProcessor
-import com.arranlomas.kontent.extensions.composeIo
+import com.arranlomas.kontent.commons.functions.KontentActionToResultProcessor
+import com.arranlomas.kontent.commons.functions.KontentSimpleNetworkRequestProcessor
+import com.arranlomas.mvisample.models.TodoItem
 import com.arranlomas.mvisample.repository.IListItemRepository
 import com.arranlomas.mvisample.todolist.objects.TodoListAction
 import com.arranlomas.mvisample.todolist.objects.TodoListResult
 import io.reactivex.Observable
-import io.reactivex.ObservableTransformer
 
-internal fun listActionProcessor(listItemRepository: IListItemRepository): ObservableTransformer<TodoListAction, TodoListResult> =
-        ObservableTransformer { action: Observable<TodoListAction> ->
-            action.publish { shared ->
-                Observable.merge(
-                        shared.ofType(TodoListAction.LoadItems::class.java).compose(loadItemsLoadProcessor(listItemRepository)),
-                        shared.ofType(TodoListAction.ChangeItemStatus::class.java).compose(changeItemStatusProcessor(listItemRepository))
-                )
-            }
-        }
-
-
-private fun loadItemsLoadProcessor(listItemRepository: IListItemRepository) = KontentSimpleProcessor<TodoListAction.LoadItems, TodoListResult> {
-    listItemRepository.getListItems()
-            .map {
-                TodoListResult.ListLoadSuccess(it) as TodoListResult
-            }
-            .onErrorReturn { TodoListResult.Error(it) }
-            .composeIo()
-            .startWith(TodoListResult.ListLoadInflight())
+/**
+ * TODO - extract this to a base component that only compiles if all actions are covered like a when
+ */
+internal fun listActionProcessor(listItemRepository: IListItemRepository) = KontentActionToResultProcessor<TodoListAction, TodoListResult> { actionObservable ->
+    Observable.merge(
+            actionObservable.ofType(TodoListAction.LoadItems::class.java).compose(loadItemsLoadProcessor(listItemRepository)),
+            actionObservable.ofType(TodoListAction.ChangeItemStatus::class.java).compose(changeItemStatusProcessor(listItemRepository))
+    )
 }
 
-private fun changeItemStatusProcessor(listItemRepository: IListItemRepository) = KontentSimpleProcessor<TodoListAction.ChangeItemStatus, TodoListResult> {
-    listItemRepository.changeItemState(it.itemId, it.itemStatus)
-            .flatMap { listItemRepository.getListItems() }
-            .map {
-                TodoListResult.ListLoadSuccess(it) as TodoListResult
-            }
-            .onErrorReturn { TodoListResult.Error(it) }
-            .composeIo()
-            .startWith(TodoListResult.ListLoadInflight())
-}
+private fun loadItemsLoadProcessor(listItemRepository: IListItemRepository) = KontentSimpleNetworkRequestProcessor<TodoListAction.LoadItems, TodoListResult, List<TodoItem>>(
+        networkRequest = { listItemRepository.getListItems() },
+        success = { TodoListResult.ListLoadSuccess(it) },
+        error = { TodoListResult.Error(it) },
+        loading = TodoListResult.ListLoadInflight())
+
+private fun changeItemStatusProcessor(listItemRepository: IListItemRepository) = KontentSimpleNetworkRequestProcessor<TodoListAction.ChangeItemStatus, TodoListResult, List<TodoItem>>(
+        networkRequest = { action ->
+            listItemRepository.changeItemState(action.itemId, action.itemStatus)
+                    .flatMap { listItemRepository.getListItems() }
+        },
+        success = { TodoListResult.ListLoadSuccess(it) },
+        error = { TodoListResult.Error(it) },
+        loading = TodoListResult.ListLoadInflight())
--- kontent/src/main/java/com/arranlomas/kontent/commons/functions/ActionProcessors.kt
+++ kontent/src/main/java/com/arranlomas/kontent/commons/functions/ActionProcessors.kt
@@ -2,12 +2,40 @@ package com.arranlomas.kontent.commons.functions
 
 import com.arranlomas.kontent.commons.objects.mvi.KontentAction
 import com.arranlomas.kontent.commons.objects.mvi.KontentResult
+import com.arranlomas.kontent.extensions.composeIo
 import io.reactivex.Observable
 import io.reactivex.ObservableTransformer
 
-fun <T: KontentAction, R: KontentResult> KontentSimpleProcessor(mapFunction: (T) -> Observable<R>):
-        ObservableTransformer<T, R> = ObservableTransformer { action ->
-    action.switchMap {
-        mapFunction.invoke(it)
+fun <T : KontentAction, R : KontentResult> KontentActionToResultProcessor(mapFunction: (Observable<T>) -> Observable<R>): ObservableTransformer<T, R> =
+        ObservableTransformer { action: Observable<T> ->
+            action.publish { shared ->
+                mapFunction.invoke(shared)
+            }
+        }
+
+fun <T : KontentAction, R : KontentResult> KontentSimpleProcessor(mapFunction: (T) -> Observable<R>): ObservableTransformer<T, R> = ObservableTransformer { action ->
+    action.switchMap { action ->
+        mapFunction.invoke(action)
     }
 }
+
+fun <T : KontentAction, R : KontentResult, O> KontentSimpleNetworkRequestProcessor(
+        networkRequest: (T) -> Observable<O>,
+        success: (O) -> R,
+        error: (Throwable) -> R,
+        loading: R
+): ObservableTransformer<T, R> = ObservableTransformer { actionObservable ->
+    actionObservable.switchMap { action ->
+        networkRequest.invoke(action)
+                .networkMapper(success, error, loading)
+    }
+}
+
+private fun <T, R : KontentResult> Observable<T>.networkMapper(success: (T) -> R, error: (Throwable) -> R, loading: R): Observable<R> = compose<R>({ observable ->
+    observable
+            .map { success.invoke(it) }
+            .onErrorReturn { error.invoke(it) }
+            .composeIo()
+            .startWith(loading)
+})
+
