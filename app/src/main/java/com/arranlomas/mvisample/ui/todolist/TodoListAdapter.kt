3e9c6a6427929add137ce0ca39581877d1916a68
arranlomas <lomasarran1993@googlemail.com>
2018-01-22 23:08:24 +1000

    remove create by arran

--- app/src/main/java/com/arranlomas/mvisample/ui/todolist/TodoListAdapter.kt
+++ app/src/main/java/com/arranlomas/mvisample/ui/todolist/TodoListAdapter.kt
@@ -9,9 +9,6 @@ import com.arranlomas.mvisample.models.TodoItemState
 import com.arranlomas.mvisample.utils.inflateLayout
 import kotlinx.android.synthetic.main.li_todo_item.view.*
 
-/**
- * Created by arran on 21/12/2017.
- */
 class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
     lateinit var onItemSelected: (Long, Boolean) -> Unit
     var items = emptyList<TodoItem>()
@@ -26,7 +23,6 @@ class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
     }
 
     override fun getItemCount(): Int = items.size
-
 }
 
 class TodoListViewHolder(itemView: View, private val onItemSelected: (Long, Boolean) -> Unit) : RecyclerView.ViewHolder(itemView) {
