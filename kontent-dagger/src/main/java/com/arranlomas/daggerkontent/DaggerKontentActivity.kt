c37213368bf6497ef508e7674a98bb6c0e36068b
arranlomas <lomasarran1993@googlemail.com>
2018-02-24 18:20:05 +1000

    add kontent dagger activity

--- kontent-dagger/src/main/java/com/arranlomas/daggerkontent/DaggerKontentActivity.kt
+++ kontent-dagger/src/main/java/com/arranlomas/daggerkontent/DaggerKontentActivity.kt
@@ -1,6 +1,64 @@
 package com.arranlomas.daggerkontent
 
+import com.arranlomas.kontent.commons.objects.KontentContract
+import com.arranlomas.kontent.commons.objects.KontentIntent
+import com.arranlomas.kontent.commons.objects.KontentViewState
+import dagger.android.DaggerActivity
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableObserver
+
 /**
  * Created by arran on 23/01/2018.
  */
-//TODO!
+abstract class DaggerKontentActivity<I : KontentIntent, S : KontentViewState> : KontentContract.View<I, S>, DaggerActivity() {
+
+    private val subscriptions = CompositeDisposable()
+    private lateinit var interactor: KontentContract.Interactor<I, S>
+    private lateinit var intents: Observable<I>
+    private var onErrorAction: ((Throwable) -> Unit)? = null
+
+    override fun setup(interactor: KontentContract.Interactor<I, S>, onErrorAction: ((Throwable) -> Unit)?) {
+        this.interactor = interactor
+        this.onErrorAction = onErrorAction
+    }
+
+    override fun setup(interactor: KontentContract.Interactor<I, S>) {
+        this.interactor = interactor
+    }
+
+
+    override fun attachIntents(intents: Observable<I>) {
+        this.intents = intents
+        interactor.attachView(intents)
+                .subscribeWith(object : BaseSubscriber<S>() {
+                    override fun onNext(state: S) {
+                        render(state)
+                    }
+                })
+                .addDisposable()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        subscriptions.dispose()
+    }
+
+    private fun Disposable.addDisposable() {
+        subscriptions.add(this)
+    }
+
+    abstract inner class BaseSubscriber<T>(val showLoading: Boolean = true) : DisposableObserver<T>() {
+        override fun onError(e: Throwable) {
+            onErrorAction?.invoke(e) ?: e.printStackTrace()
+        }
+
+        override fun onStart() {
+        }
+
+        override fun onComplete() {
+
+        }
+    }
+}
