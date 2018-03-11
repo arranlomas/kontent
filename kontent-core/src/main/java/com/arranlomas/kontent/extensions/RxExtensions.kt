1473e11b4e4a2cf94c1d00340d946ab12d8aad90
arranlomas <lomasarran1993@googlemail.com>
2018-03-11 23:00:55 +1000

    cleanup notOfTypeMethod

--- kontent-core/src/main/java/com/arranlomas/kontent/extensions/RxExtensions.kt
+++ kontent-core/src/main/java/com/arranlomas/kontent/extensions/RxExtensions.kt
@@ -13,7 +13,7 @@ fun <T> Observable<T>.composeIo(): Observable<T> = compose<T>({
 
 @CheckReturnValue
 @SchedulerSupport(SchedulerSupport.NONE)
-fun <T : Any, U : Any> Observable<T>.notOfType(clazz: Class<U>): Observable<T> {
+fun <T, U > Observable<T>.notOfType(clazz: Class<U>): Observable<T> {
     checkNotNull(clazz) { "clazz is null" }
     return filter { !clazz.isInstance(it) }
 }
